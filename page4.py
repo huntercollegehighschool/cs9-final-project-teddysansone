90a48922a0d7dd188a47cff22435346b76f01b10
Theodore Sansone <104437039+teddysansone@users.noreply.github.com>
2022-06-16 19:51:25 +0000

    finished

--- page4.py
+++ page4.py
@@ -7,7 +7,7 @@ NOUN2 = input("Enter a noun: ")
 ADJECTIVE2 = input("Enter an adjective: ")
 PLNOUN2 = input("Enter a plural noun: ")
 VERB2 = input("Enter a verb: ")
-ADJECTIVE3 = input("Enter an adjective: ")
+ADJECTIVE3 = input("Enter a Verb: ")
 
 
 print("It was a " + ADJECTIVE1 + " day at the beach. First, we went in the " + ADJECTIVE2 + " water. We had fun " + VERB + " in the water. Then, we dried off and ate " + PLNOUN + ". After, I made a " + NOUN + " castle. when the sun set, I made a " + NOUN2 + " fire and roasted " + ADJECTIVE2+ " " + PLNOUN2 + ". Finally it was time to " + VERB2 + " home. I had alot of " + ADJECTIVE3 + ".")
