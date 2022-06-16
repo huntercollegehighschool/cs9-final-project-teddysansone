bfe0127670bace23b56d2537a2ea2f4a4e06b92e
Theodore Sansone <104437039+teddysansone@users.noreply.github.com>
2022-06-16 12:57:00 +0000

    work

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 #repl"""
 #Name(s): teddy sansone
 #name of Project: Mad libs
-"""
 program = int(input("Which part are you testing? Type in 1, 2, 3, 4, or 5 : "))
 
 if program == 1:
-  import part1
+  import page1
 elif program == 2:
-  import part2
+  import page2
 elif program == 3:
-  import part3
+  import page3
 elif program == 4:
-  import part4
+  import page4
 elif program == 5:
-  import part5
-#Write the main part of your program here. Use of the other pages is optional.
-
-#import page1  # uncomment if you're using page1
-#import page2  # uncomment if you're using page2
-#import page3  # uncomment if you're using page3
-#import page4  # uncomment if you're using page4
+  import page5
--- page2.py
+++ page2.py
@@ -8,4 +8,4 @@ noun4 = input("enter a noun")
 sport = input("enter a sport")
 
 
-print("First I woke up up at {time). I ate {noun1} for breakfast. I took the {noun2} to school. My first class was {verb1). Then it was time for lunch, i ate {noun3}. At recess, we play {game}. then i had {noun4} class. Finally, I went to SPORT practice and then went home.")
+print("First I woke up up at + time. I ate {noun1} for breakfast. I took the {noun2} to school. My first class was {verb1). Then it was time for lunch, i ate {noun3}. At recess, we play {game}. then i had {noun4} class. Finally, I went to SPORT practice and then went home.)
